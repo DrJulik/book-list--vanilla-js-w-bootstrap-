9b6d4b294d1ab0887fc845577787932efc0c6e7a
Julius Sozin <J.Batkovich@gmail.com>
2019-05-22 21:08:00 -0400

    Fixed issues. added features. finished for now

---
+++ Readme.md
@@ -0,0 +1,19 @@
+# The Book Tracker App
+
+## What is this project?
+
+This is a classic Vanilla JS project "The Book List App". However, this one has many features added on top of the regular "add book and delete book".
+
+## Features of this book list:
+
+You can search the list by title, author or isbn. You can press on the headers in the list to order them alphabetically or numerically.
+Upon the addition of the book, the app generates a link to the Goodreads page of that book (provided you have entered correct ISBN).
+The App also features an autocomplete feature, which takes items from a JSON file and displays them using `<datalist>` (This feature is here to simulate how an app would behave if it had a cool database with all the books).
+
+### Me
+
+If you have any questions, don't hesitate to ask!
+
+Yuliy
+
+yuliysozin.dev
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 class Book {
-  constructor (title, author, isbn,read) {
+  constructor(title, author, isbn, read) {
     this.title = title;
     this.author = author;
     this.isbn = isbn;
@@ -8,13 +8,12 @@ class Book {
 }
 
 class UI {
-  addBookToList(book){
-
+  addBookToList(book) {
     // read checkbox conversion
     if (book.read === true) {
-      book.read = "Yes"
+      book.read = 'Yes';
     } else {
-      book.read = "No"
+      book.read = 'No';
     }
 
     const list = document.getElementById('book-list');
@@ -28,41 +27,43 @@ class UI {
     <td class="item">${book.author}</td>
     <td class="item">${book.isbn}</td>
     <td class="item">${book.read}</td>
+    <td><a href="https://www.goodreads.com/book/isbn/${
+      book.isbn
+    }" target="_blank"><img src="https://s.gr-assets.com/assets/icons/goodreads_icon_32x32-6c9373254f526f7fdf2980162991a2b3.png"></img><a></td>
     <td><a href="#" class="delete">X<a></td>
+    
     `;
 
     list.appendChild(row);
-  
   }
 
   showAlert(message, className) {
     // create div
-  const div = document.createElement('div');
-  // add classes
-  div.className = `alert ${className}`;
-  // add text
-  div.appendChild(document.createTextNode(message));
-  // get parent
-  const container = document.querySelector('.container');
+    const div = document.createElement('div');
+    // add classes
+    div.className = `alert ${className}`;
+    // add text
+    div.appendChild(document.createTextNode(message));
+    // get parent
+    const container = document.querySelector('.container');
 
-  const form = document.querySelector('#book-form');
+    const form = document.querySelector('#book-form');
 
-  container.insertBefore(div, form);
+    container.insertBefore(div, form);
 
-  // timeout after 3s
-  setTimeout(function(){
-    document.querySelector('.alert').remove();
-  }, 3000)
+    // timeout after 3s
+    setTimeout(function() {
+      document.querySelector('.alert').remove();
+    }, 3000);
   }
 
-  deleteBook(target){
-    if(target.className === 'delete'){
+  deleteBook(target) {
+    if (target.className === 'delete') {
       target.parentElement.parentElement.remove();
     }
-}
+  }
 
-  filterBooks(n,inputField) {
-
+  filterBooks(n, inputField) {
     function filterB() {
       // vars
       let i,
@@ -73,26 +74,25 @@ class UI {
 
       // loop through rows
       for (i = 0; i < bookRow.length; i++) {
-        let td = bookRow[i].getElementsByClassName("item")[n];
+        let td = bookRow[i].getElementsByClassName('item')[n];
 
-        
-      if (td) {
-        let txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        bookRow[i].style.display = "";
-      } else {
-        bookRow[i].style.display = "none";
+        if (td) {
+          let txtValue = td.textContent || td.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            bookRow[i].style.display = '';
+          } else {
+            bookRow[i].style.display = 'none';
+          }
+        }
       }
-      }
-    }
     }
     filterB();
-}
+  }
 
-sortBooks(n) {
-  function sortT(){
-    // vars
-    let table = document.getElementById('table'),
+  sortBooks(n) {
+    function sortT() {
+      // vars
+      let table = document.getElementById('table'),
         i,
         x,
         y,
@@ -100,93 +100,91 @@ sortBooks(n) {
         switchCount = 0,
         rows = document.getElementsByClassName('book-entry'),
         switching = true;
-    //sorting direction to ascending:
-    let dir = "asc"; 
-    /* Make a loop that will continue until
+      //sorting direction to ascending:
+      let dir = 'asc';
+      /* Make a loop that will continue until
     no switching has been done: */
-    while (switching) {
-      // Start by saying: no switching is done:
-      switching = false;
-      /* Loop through all table rows (except the
+      while (switching) {
+        // Start by saying: no switching is done:
+        switching = false;
+        /* Loop through all table rows (except the
       first, which contains table headers): */
-      for (i = 0; i < (rows.length - 1); i++) {
-        // Start by saying there should be no switching:
-        shouldSwitch = false;
-        /* Get the two elements you want to compare,
+        for (i = 0; i < rows.length - 1; i++) {
+          // Start by saying there should be no switching:
+          shouldSwitch = false;
+          /* Get the two elements you want to compare,
         one from current row and one from the next: */
-        x = rows[i].getElementsByClassName("item")[n];
-        y = rows[i + 1].getElementsByClassName("item")[n];
-        /* Check if the two rows should switch place,
+          x = rows[i].getElementsByClassName('item')[n];
+          y = rows[i + 1].getElementsByClassName('item')[n];
+          /* Check if the two rows should switch place,
         based on the direction, asc or desc: */
-        if (dir == "asc") {
-          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-            // If so, mark as a switch and break the loop:
-            shouldSwitch = true;
-            break;
-          }
-        } else if (dir == "desc") {
-          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-            // If so, mark as a switch and break the loop:
-            shouldSwitch = true;
-            break;
+          if (dir == 'asc') {
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+              // If so, mark as a switch and break the loop:
+              shouldSwitch = true;
+              break;
+            }
+          } else if (dir == 'desc') {
+            if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+              // If so, mark as a switch and break the loop:
+              shouldSwitch = true;
+              break;
+            }
           }
         }
-      }
-      if (shouldSwitch) {
-        /* If a switch has been marked, make the switch
+        if (shouldSwitch) {
+          /* If a switch has been marked, make the switch
         and mark that a switch has been done: */
-        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-        switching = true;
-        // Each time a switch is done, increase this count by 1:
-        switchCount ++; 
-      } else {
-        /* If no switching has been done AND the direction is "asc",
-        set the direction to "desc" and run the while loop again. */
-        if (switchCount == 0 && dir == "asc") {
-          dir = "desc";
+          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
           switching = true;
+          // Each time a switch is done, increase this count by 1:
+          switchCount++;
+        } else {
+          /* If no switching has been done AND the direction is "asc",
+        set the direction to "desc" and run the while loop again. */
+          if (switchCount == 0 && dir == 'asc') {
+            dir = 'desc';
+            switching = true;
+          }
         }
       }
     }
-  } 
-  sortT();
-    }
+    sortT();
+  }
 
-  
-
-clearFields() {
-  document.getElementById('title').value = '';
-  document.getElementById('author').value = '';
-  document.getElementById('isbn').value = '';
-  document.getElementById('read').checked = false;
+  clearFields() {
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+    document.getElementById('read').checked = false;
   }
 }
 
 // local storage class
 class Store {
-  static getBooks(){
+  static getBooks() {
     let books;
-    if(localStorage.getItem('books') === null){
+    if (localStorage.getItem('books') === null) {
       books = [];
     } else {
       books = JSON.parse(localStorage.getItem('books'));
     }
 
-    return books; 
+    return books;
   }
 
   static displayBooks() {
     const books = Store.getBooks();
 
-    books.forEach(function(book){
-      const ui = new UI;
+    books.forEach(function(book) {
+      const ui = new UI();
 
       // add book to UI
       ui.addBookToList(book);
     });
   }
 
-  static addBook(book){
+  static addBook(book) {
     const books = Store.getBooks();
 
     books.push(book);
@@ -194,105 +192,99 @@ class Store {
     localStorage.setItem('books', JSON.stringify(books));
   }
 
-  static removeBook(isbn){
+  static removeBook(isbn) {
     const books = Store.getBooks();
 
-    books.forEach(function(book,index){
-    if(book.isbn === isbn){
-      books.splice(index, 1);
-    }
-  });
+    books.forEach(function(book, index) {
+      if (book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    });
 
-  localStorage.setItem('books', JSON.stringify(books));
-}
+    localStorage.setItem('books', JSON.stringify(books));
+  }
 }
 // DOM load event
 document.addEventListener('DOMContentLoaded', Store.displayBooks);
 // Event Listeners for addBook
 
-document.getElementById('book-form').addEventListener('submit', function(e){
+document.getElementById('book-form').addEventListener('submit', function(e) {
   const title = document.getElementById('title').value,
-        author = document.getElementById('author').value,
-        isbn = document.getElementById('isbn').value,
-        read = document.getElementById('read').checked;
+    author = document.getElementById('author').value,
+    isbn = document.getElementById('isbn').value,
+    read = document.getElementById('read').checked;
 
   const book = new Book(title, author, isbn, read);
 
   const ui = new UI();
 
-  // validate 
-  if(title === '' || author === '' || isbn === '') {
+  // validate
+  if (title === '' || author === '' || isbn === '') {
     // error alert
     ui.showAlert('Please fill in all fields', 'error');
   } else {
-  // Add book to list
-  ui.addBookToList(book);
+    // Add book to list
+    ui.addBookToList(book);
 
-  // add to local storage
-  Store.addBook(book);
+    // add to local storage
+    Store.addBook(book);
 
-  // show success
-  ui.showAlert('Book added!', 'success');
+    // show success
+    ui.showAlert('Book added!', 'success');
 
-  // clear fields
-  ui.clearFields();
+    // clear fields
+    ui.clearFields();
+  }
 
-  } 
-
-  
   e.preventDefault();
 });
 
-
 // search for Book title
-document.getElementById('filter-title').addEventListener('keyup', function(){
-  const ui = new UI;
+document.getElementById('filter-title').addEventListener('keyup', function() {
+  const ui = new UI();
   ui.filterBooks(0, document.getElementById('filter-title'));
-})
+});
 
 // search for Book title
-document.getElementById('filter-author').addEventListener('keyup', function(){
-  const ui = new UI;
+document.getElementById('filter-author').addEventListener('keyup', function() {
+  const ui = new UI();
   ui.filterBooks(1, document.getElementById('filter-author'));
-})
+});
 
 // search for Book title
-document.getElementById('filter-isbn').addEventListener('keyup', function(){
-  const ui = new UI;
+document.getElementById('filter-isbn').addEventListener('keyup', function() {
+  const ui = new UI();
   ui.filterBooks(2, document.getElementById('filter-isbn'));
-})
-
-
-
-
+});
 
 // sort books by title
-document.getElementById('book-title').addEventListener('click', function(){
-  const ui = new UI;
+document.getElementById('book-title').addEventListener('click', function() {
+  const ui = new UI();
   ui.sortBooks(0);
-})
+});
 // sort books by author
-document.getElementById('book-author').addEventListener('click', function(){
-  const ui = new UI;
+document.getElementById('book-author').addEventListener('click', function() {
+  const ui = new UI();
   ui.sortBooks(1);
-})
+});
 // sort books by isbn
-document.getElementById('book-isbn').addEventListener('click', function(){
-  const ui = new UI;
+document.getElementById('book-isbn').addEventListener('click', function() {
+  const ui = new UI();
   ui.sortBooks(2);
-})
-
+});
 
 // event listener for delete
-document.getElementById('book-list').addEventListener('click', function(e){
-
+document.getElementById('book-list').addEventListener('click', function(e) {
   const ui = new UI();
 
   // delete book
   ui.deleteBook(e.target);
 
   // remove from local storage
-  Store.removeBook(e.target.parentElement.previousElementSibling.previousElementSibling.textContent);
+  Store.removeBook(
+    e.target.parentElement.previousElementSibling.previousElementSibling
+      .previousElementSibling.textContent
+  );
 
   // show message
   ui.showAlert('Book removed', 'success');
@@ -300,4 +292,42 @@ document.getElementById('book-list').addEventListener('click', function(e){
   e.preventDefault();
 });
 
+// search the json and filter through
+const title = document.getElementById('title');
+const matchList = document.getElementById('titles');
+title.addEventListener('input', () => searchBooks(title.value));
 
+const searchBooks = async searchText => {
+  const res = await fetch('books.json');
+  const books = await res.json();
+
+  // match to current input
+  let matches = books.filter(book => {
+    const regex = new RegExp(`^${searchText}`, 'gi');
+    return book.title.match(regex);
+  });
+
+  if (searchText.length === 0) {
+    matches = [];
+    matchList.innerHTML = '';
+  }
+
+  outputHtml(matches);
+};
+
+// output html
+const outputHtml = matches => {
+  if (matches.length > 0) {
+    const html = matches
+      .map(
+        match => `
+    <option value="${match.title}">
+    `
+      )
+      .join('');
+
+    console.log(html);
+
+    matchList.innerHTML = html;
+  }
+};
